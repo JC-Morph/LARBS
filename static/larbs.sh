cfb0c786bccd995d69ff7c74eaa0ff321038bb02
JC-Morph <jc_morph@caramail.com>
2026-07-04 19:24:19 +0100

    Redirect script and scrub rss

--- static/larbs.sh
+++ static/larbs.sh
@@ -6,22 +6,14 @@
 
 ### OPTIONS AND VARIABLES ###
 
-dotfilesrepo="https://github.com/lukesmithxyz/voidrice.git"
-progsfile="https://raw.githubusercontent.com/LukeSmithxyz/LARBS/master/static/progs.csv"
-aurhelper="yay"
+dotfilesrepo="https://github.com/jc-morph/voidrice.git"
+progsfile="https://raw.githubusercontent.com/JC-Morph/LARBS/master/static/progs.csv"
+aurhelper="paru"
 repobranch="master"
 export TERM=ansi
 
-rssurls="https://lukesmith.xyz/rss.xml
-https://videos.lukesmith.xyz/feeds/videos.xml?videoChannelId=2 \"~Luke Smith (Videos)\"
-https://www.youtube.com/feeds/videos.xml?channel_id=UC2eYFnH61tmytImy1mTYvhA \"~Luke Smith (YouTube)\"
-https://lindypress.net/rss
-https://notrelated.xyz/rss
-https://landchad.net/rss.xml
-https://based.cooking/index.xml
-https://artixlinux.org/feed.php \"tech\"
-https://www.archlinux.org/feeds/news/ \"tech\"
-https://github.com/LukeSmithxyz/voidrice/commits/master.atom \"~LARBS dotfiles\""
+rssurls="https://artixlinux.org/feed.php \"tech\"
+https://www.youtube.com/feeds/videos.xml?channel_id=UC_h1nkWkxN0_OcRdzrKNIug \"~[YT] Wouter van Veldhoven\""
 
 ### FUNCTIONS ###
 
